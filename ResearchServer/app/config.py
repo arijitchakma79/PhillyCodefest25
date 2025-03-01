22977cecd2cb93c5b18b687a2fc7ba5fb6017c6c
arijitchakma79 <arijitchakma79@gmail.com>
2025-03-01 14:54:08 -0500

    added google trend analyzer

--- ResearchServer/app/config.py
+++ ResearchServer/app/config.py
@@ -9,5 +9,7 @@ load_dotenv(dotenv_path)
 class Config:
     """Configuration settings for Flask app"""
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    SERP_API_KEY = os.getenv("SERP_API_KEY")
+    NEWS_API_KEY = os.getenv("NEWS_API_KEY")
     DEBUG = True
 
